b5cf3a1dee91fd5972a2951d0bf7064a474e679b
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2015-09-09 11:42:45 -0300

    Update Global.asax.cs

--- FaleMaizTelZir.Mvc/Global.asax.cs
+++ FaleMaizTelZir.Mvc/Global.asax.cs
@@ -8,7 +8,7 @@ using System.Web.Routing;
 
 namespace FaleMaizTelZir.Mvc
 {
-    public sadas MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
