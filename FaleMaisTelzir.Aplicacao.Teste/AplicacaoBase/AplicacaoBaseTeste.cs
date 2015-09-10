27e17dbf92b0b42fae9e0ed093f03f75668bac50
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2015-09-10 10:37:45 -0300

    Update AplicacaoBaseTeste.cs

--- FaleMaisTelzir.Aplicacao.Teste/AplicacaoBase/AplicacaoBaseTeste.cs
+++ FaleMaisTelzir.Aplicacao.Teste/AplicacaoBase/AplicacaoBaseTeste.cs
@@ -23,10 +23,11 @@ namespace FaleMaisTelzir.Aplicacao.Teste.AplicacaoBase
             var aplicacao = new AplicacaoBase<Chamada>(mockServico.Object);
 
             chamada = aplicacao.Salvar(chamada);
-
+            
+            Assert.Fail();
             mockServico.Verify(x => x.Inserir(It.IsAny<Chamada>()), Times.Once());
             mockServico.Verify(x => x.Alterar(It.IsAny<Chamada>()), Times.Never());
-            Assert.AreEqual(78, chamada.Codigo);
+            Assert.AreEqual(34, chamada.Codigo);
 
         }
 
